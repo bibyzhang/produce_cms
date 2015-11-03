bd849527f7ec1404c6b0d63b040df049444ebe6c
biby <zhangbibin@appgame.com>
2015-11-03 17:14:11 +0800

    basemodel

--- model/BaseModel.class.php
+++ model/BaseModel.class.php
@@ -1,17 +1,26 @@
 <?php
 // +----------------------------------------------------------------------
-// |
+// | @name BaseModel
 // +----------------------------------------------------------------------
-// | 基本模型
+// | @desc 基本模型,所有模型文件继承基本模型
+// +----------------------------------------------------------------------
+// | @author bibyzhang90@gmail.com
 // +----------------------------------------------------------------------
 
 namespace model;
 
-defined('_ACCESS_GRANT') or exit('没有访问权限!');
+defined('ACCESS_GRANT') or exit('没有访问权限!');
 
 class BaseModel extends \common\libs\classes\Base{
 
-    public function __construct(){
-        parent::__construct();
+    public function __construct() {
+    }
+
+    public function selectSample() {
+        return 'Hello World!';
+    }
+
+    public function insertSample($arrInfo) {
+        return true;
     }
 }
